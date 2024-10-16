6b46e88081b8c63535efd46744fb8abdff67092c
NovalAdiyaPutra <12350112933@students.uin-suska.ac.id>
2024-10-16 08:52:14 +0700

    first

--- src/PlaylistLagu/AppPlaylist.java
+++ src/PlaylistLagu/AppPlaylist.java
@@ -9,6 +9,12 @@ public class AppPlaylist {
         lagu.addSong("Magnetic - Illit");
         lagu.addSong("Love Arcade - Red Velvet");
         lagu.addSong("Talk That Talk - Twice");
+        lagu.addSong("Wa Da Da - Kep1er");
+        lagu.addSong("Cupid - Fifty Fifty");
+        lagu.addSong("How You Like That - BlackPink");
+        lagu.addSong("Mago - Gfriend");
+        lagu.addSong("How You Like That - BlackPink");
+        lagu.addSong("Dice - Nmixx");
 
         lagu.displayPlaylist();
     }
